dcbe7a511f0e8cd01f07e964f4401e3d77cf7f74
AlexanderGhosh <ghoshalexander@gmail.com>
2023-12-12 21:41:38 +0000

    working 8ms

--- DirectLifeTest/Program.cs
+++ DirectLifeTest/Program.cs
@@ -1,15 +1,82 @@
-﻿namespace DirectLifeTest
+﻿
+namespace DirectLifeTest
 {
     internal class Program
     {
-        private const string FilePath = "";
+        // returns true if string 'a' would be in a lower index than string 'b' if sorted alphabeticaly
+        private static bool greaterThan(string a, string b)
+        {
+            return string.Compare(a, b) >= 0;
+        }
+        // returns the strings value as descriped in the brief (doesnt do the multiply by position)
+        private static int getValue(string a)
+        {
+            int total = 0;
+            // casts to lower to garuntee the to value calculation works consistantly
+            a = a.ToLower();
 
+            foreach (char c in a)
+            {
+                // deducts 96 because 'a' holds the value of 97 in the ascii table and we want it to have the value of 1
+                // given all lowercase letters are listed in order sequentialy this will convert all letters to its location in the alphabet
+                total += c - 96;
+            }
+            return total;
+        }
         private static void Main(string[] args)
         {
-            foreach (string line in File.ReadAllLines(FilePath))
-            {
-                Console.WriteLine(line);
+            var watch = new System.Diagnostics.Stopwatch();
+            int num_itter = 100;
+
+            watch.Start();
+
+            for (int itter = 0; itter < num_itter; itter++)
+            {// unless this line is altered it is asumed that the "names.txt" is located in the same directory as the ".exe"
+                string FilePath = $".\\names.txt";
+
+                // maps the word to the ammount of times it was seen
+                SortedDictionary<string, int> sorted = [];
+
+                using (var file = new StreamReader(FilePath)) // this will open the file for reading and closes it at the end of the "using" block
+                {
+                    string text = file.ReadToEnd();
+                    if (text.Length <= 0)
+                    {
+                        // exits if no text found
+                        return;
+                    }
+                    var split = text.Split("\",\""); // removes the dilimeters and all but the first and last "
+
+                    int size = split.Length;
+                    int lastIndex = size - 1; // just for my conviniance
+
+                    // replaces ernouiouse "
+                    split[0] = split[0].TrimStart('"'); // doesnt do replace because unless the word contains charaters out side of the english alphabet there will be no other occurances
+                    split[lastIndex] = split[lastIndex].TrimEnd('"');
+
+                    foreach (string a in split)
+                    {
+                        if (!sorted.TryAdd(a, 1))
+                        {
+                            ++sorted[a];
+                        }
+                    }
+                }
+                int total = 0;
+                int idx = 0;
+                foreach (var word in sorted)
+                {
+                    total += getValue(word.Key) * word.Value * ++idx;
+                }
+
+                // Console.WriteLine($"The total value of the file is: {total}");
             }
+
+
+
+            watch.Stop();
+
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds / num_itter} ms");
         }
     }
 }
